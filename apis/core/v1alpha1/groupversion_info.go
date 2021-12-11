dd3f8432612c86d14db000ba1c263e0f9b012157
went <went@went.local>
2021-12-11 18:29:16 +0800

    keep both GroupVersion and SchemeGroupVersion

--- apis/core/v1alpha1/groupversion_info.go
+++ apis/core/v1alpha1/groupversion_info.go
@@ -25,11 +25,15 @@ import (
 )
 
 var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "core.openfunction.io", Version: "v1alpha1"}
+
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "core.openfunction.io", Version: "v1alpha1"}
+	// added for generated clientset
+	SchemeGroupVersion = GroupVersion
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
--- apis/core/v1alpha2/groupversion_info.go
+++ apis/core/v1alpha2/groupversion_info.go
@@ -25,11 +25,15 @@ import (
 )
 
 var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "core.openfunction.io", Version: "v1alpha2"}
+
 	// SchemeGroupVersion is group version used to register these objects
-	SchemeGroupVersion = schema.GroupVersion{Group: "core.openfunction.io", Version: "v1alpha2"}
+	// added for generated clientset
+	SchemeGroupVersion = GroupVersion
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: SchemeGroupVersion}
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
